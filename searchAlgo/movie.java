912d2c19a0747a8d3366798cb6fb0095c1d54558
debo <yliang2@uw.edu>
2020-02-03 14:25:26 -0800

    routine update

--- searchAlgo/movie.java
+++ searchAlgo/movie.java
@@ -9,29 +9,36 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 
-public class movie extends Jpanel implements ActionListener{
-    Timer tm  = new Timer(5,this);
+public class movie extends JPanel implements ActionListener{
+    Timer tm  = new Timer(1,this);   /*this stands for the movie*/
+    System.out.println(this);
     int x = 0, velx = 2;
-    public void paintComponent(Graphic g){
-        super.paintComponet(g);
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
         g.setColor(Color.RED);
-        g.fillrect(x,30,50,30);
+        g.fillRect(x,x,50,30);
 
         tm.start();
     }
 
     public void actionPerformed(ActionEvent e){
+        if(x<0 || x>550){
+            velx = -velx;
+        }
         x = x + velx;
         repaint();
     }
 
-    public void main(String args[]){
+    public static void main(String args[]){
         movie m = new movie();
         JFrame jf = new JFrame();
         jf.setTitle("Kind of Movie ");
-        jf.setSize(600,400);
+        jf.setSize(600,600);
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.add(m);
     }
 }
+
+
